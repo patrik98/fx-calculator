2a51e02f63a151c85e618dd331aefa7ac961e593
Patrik Golec <2010738979@stud.fh-kufstein.ac.at>
2021-04-26 15:20:11 +0200

    added PWA install prompt and basic functionality

--- docs/app.js
+++ docs/app.js
@@ -9,7 +9,7 @@ document.querySelectorAll('select').forEach(element => {
     `
 });
 
-document.querySelector('button').addEventListener('click', async () => {
+document.querySelector('#btnCalc').addEventListener('click', async () => {
     const inputCurrency = document.querySelector('[name="input-currency"]').value;
     const outputCurrency = document.querySelector('[name="output-currency"]').value;
 
@@ -17,9 +17,8 @@ document.querySelector('button').addEventListener('click', async () => {
     const outputValue = await convert(inputValue, inputCurrency, outputCurrency);
 
     document.querySelector('[name="output-value"]').value = round(outputValue, 2);
-});
+}); 
 
-// const fixerUri = 'https://data.fixer.io/api/latest?base=EUR&symbols=USD,SEK,CHF&access_key=API_KEY';
 const fixerUri = 'fixer.json';
 
 async function convert(inputValue, inputCurrency, outputCurrency) {
@@ -33,3 +32,47 @@ async function convert(inputValue, inputCurrency, outputCurrency) {
 function round(value, decimals) {
     return (Math.round(value * 100) / 100).toFixed(decimals);
 }
+
+
+// PWA-related install prompt
+
+const prompt = document.querySelector('article');
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    prompt.style['display'] = 'inline';
+});
+
+window.addEventListener('appinstalled', () => {    
+    prompt.style['display'] = 'none';
+    deferredPrompt = null;
+    updatePWADisplayMode();
+});
+
+prompt.addEventListener('click', function(event) {
+    if (event.target.dataset.id == 'install-yes' && deferredPrompt) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then(result => {
+            console.log("result of user prompt", result);
+            prompt.style['display'] = 'none';
+            deferredPrompt = null;
+            updatePWADisplayMode();
+        });        
+    } else {
+        updatePWADisplayMode();
+        prompt.style['display'] = 'none';
+    }
+});
+
+function updatePWADisplayMode() {
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+    if (document.referrer.startsWith('android-app://')) {
+        displayMode = 'twa'; // Trusted Web Activity
+    } else if (navigator.standalone || isStandalone) { // navigator.standalone iOS hack
+        displayMode = 'standalone';
+    }
+}
+
+updatePWADisplayMode();
