61b1b7e0a5c1f8b53a4b407d07dc913a8c197379
Patrik Golec <2010738979@stud.fh-kufstein.ac.at>
2021-04-26 14:12:14 +0200

    added eventListener on button for conversion

--- docs/app.js
+++ docs/app.js
@@ -9,6 +9,16 @@ document.querySelectorAll('select').forEach(element => {
     `
 });
 
+document.querySelector('button').addEventListener('click', async () => {
+    const inputCurrency = document.querySelector('[name="input-currency"]').value;
+    const outputCurrency = document.querySelector('[name="output-currency"]').value;
+
+    const inputValue = document.querySelector('[name="input-value"]').value;
+    const outputValue = await convert(inputValue, inputCurrency, outputCurrency);
+
+    document.querySelector('[name="output-value"]').value = round(outputValue, 2);
+});
+
 // const fixerUri = 'https://data.fixer.io/api/latest?base=EUR&symbols=USD,SEK,CHF&access_key=API_KEY';
 const fixerUri = 'fixer.json';
 
