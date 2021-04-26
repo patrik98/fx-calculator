59d87d2e6e6552688666273b919f61e94f0cc6a9
Patrik Golec <2010738979@stud.fh-kufstein.ac.at>
2021-04-26 14:07:46 +0200

    added function for conversion of currencies and rounding

--- docs/app.js
+++ docs/app.js
@@ -10,4 +10,16 @@ document.querySelectorAll('select').forEach(element => {
 });
 
 // const fixerUri = 'https://data.fixer.io/api/latest?base=EUR&symbols=USD,SEK,CHF&access_key=API_KEY';
-const fixerUri = "fixer.json";
+const fixerUri = 'fixer.json';
+
+async function convert(inputValue, inputCurrency, outputCurrency) {
+    const response = await fetch(fixerUri);
+    const data = await response.json();
+    const rates = data['rates'];
+    rates['EUR'] = 1.;
+    return inputValue / rates[inputCurrency] * rates[outputCurrency];
+}
+
+function round(value, decimals) {
+    return (Math.round(value * 100) / 100).toFixed(decimals);
+}
