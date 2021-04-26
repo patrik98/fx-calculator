de5365864869369c071f1ed52d0a9e68c2365e8b
Patrik Golec <2010738979@stud.fh-kufstein.ac.at>
2021-04-26 13:56:07 +0200

    added currency options to select elements

---
+++ docs/app.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('select').forEach(element => {
+    element.innerHTML = 
+    `
+    <option value="EUR">EUR</option>
+    <option value="USD">USD</option>
+    <option value="CHF">CHF</option>
+    <option value="SEK">SEK</option>
+    <option value="BTC">BTC</option>
+    `
+});
